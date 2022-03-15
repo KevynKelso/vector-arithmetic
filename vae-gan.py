99529ef38cab5f50bc3e2a6bcc9cf9b7f630c543
Kevyn Kelso <vyn@Kevyns-MacBook-Pro.local>
2022-03-15 06:48:28 -0600

    feat: change VAE to AE

--- vae-gan.py
+++ vae-gan.py
@@ -10,8 +10,92 @@ from tensorflow.keras.optimizers import Adam
 from gan import (define_discriminator, define_gan, generate_real_samples,
                  load_real_samples)
 
+# def vae():
+# latent_dim = 128  # Number of latent dimension parameters
+# input_img = Input(shape=(80, 80, 3))
 
-def vae():
+# x = Conv2D(128, (3, 3), activation="relu", padding="same", strides=2)(input_img)
+# x = BatchNormalization()(x)
+
+# x = Conv2D(128, (3, 3), activation="relu", padding="same", strides=2)(x)
+# x = BatchNormalization()(x)
+
+# x = Conv2D(128, (3, 3), activation="relu", padding="same", strides=2)(x)
+# x = BatchNormalization()(x)
+
+# x = Conv2D(128, (3, 3), activation="relu", padding="same", strides=2)(x)
+# x = BatchNormalization()(x)
+
+# # shape_before_flattening = K.int_shape(x)
+# x = Flatten()(x)
+
+# z_mu = Dense(latent_dim, name="z_mu")(x)
+# z_log_sigma = Dense(
+# latent_dim,
+# kernel_initializer="zeros",
+# bias_initializer="zeros",
+# name="z_log_sigma",
+# )(x)
+
+# # sampling function
+# def sampling(args):
+# z_mu, z_log_sigma = args
+# epsilon = K.random_normal(
+# shape=(K.shape(z_mu)[0], latent_dim), mean=0.0, stddev=1.0
+# )
+# return z_mu + K.exp(z_log_sigma) * epsilon
+
+# # sample vector from the latent distribution
+# z = Lambda(sampling)([z_mu, z_log_sigma])
+
+# # encoder = Model(input_img, z)
+# # encoder.summary()
+# # decoder takes the latent distribution sample as input
+# decoder_input = Input(K.int_shape(z)[1:])
+# x = Dense(
+# 3200, activation="relu", name="intermediate_decoder", input_shape=(latent_dim,)
+# )(decoder_input)
+# x = Reshape((5, 5, 128))(x)
+
+# x = Conv2DTranspose(128, (3, 3), strides=2, padding="same")(x)
+# x = BatchNormalization()(x)
+
+# x = Conv2DTranspose(128, (3, 3), strides=2, padding="same")(x)
+# x = BatchNormalization()(x)
+
+# x = Conv2DTranspose(128, (3, 3), strides=2, padding="same")(x)
+# x = BatchNormalization()(x)
+
+# x = Conv2DTranspose(3, (3, 3), strides=2, padding="same", activation="sigmoid")(x)
+
+# # decoder model statement
+# decoder = Model(decoder_input, x)
+
+# # apply the decoder to the sample from the latent distribution
+# pred = decoder(z)
+
+# def vae_loss(x, pred):
+# x = K.flatten(x)
+# pred = K.flatten(pred)
+# # Reconstruction loss
+# reconst_loss = 1000 * K.mean(K.square(x - pred))
+
+# # KL divergence
+# kl_loss = -0.5 * K.mean(
+# 1 + z_log_sigma - K.square(z_mu) - K.exp(z_log_sigma), axis=-1
+# )
+
+# return reconst_loss + kl_loss
+
+# # VAE model statement
+# vae = Model(input_img, pred, name="VAE")
+# vae.add_loss(vae_loss(input_img, pred))
+# # optimizer = Adam(learning_rate=0.0005)
+# # vae.compile(optimizer=optimizer, loss=None)
+
+# # vae.summary()
+# return vae
+def ae():
     latent_dim = 128  # Number of latent dimension parameters
     input_img = Input(shape=(80, 80, 3))
 
@@ -30,32 +114,11 @@ def vae():
     # shape_before_flattening = K.int_shape(x)
     x = Flatten()(x)
 
-    z_mu = Dense(latent_dim, name="z_mu")(x)
-    z_log_sigma = Dense(
-        latent_dim,
-        kernel_initializer="zeros",
-        bias_initializer="zeros",
-        name="z_log_sigma",
-    )(x)
+    x = Dense(latent_dim, activation="relu")(x)
 
-    # sampling function
-    def sampling(args):
-        z_mu, z_log_sigma = args
-        epsilon = K.random_normal(
-            shape=(K.shape(z_mu)[0], latent_dim), mean=0.0, stddev=1.0
-        )
-        return z_mu + K.exp(z_log_sigma) * epsilon
-
-    # sample vector from the latent distribution
-    z = Lambda(sampling)([z_mu, z_log_sigma])
-
-    # encoder = Model(input_img, z)
-    # encoder.summary()
-    # decoder takes the latent distribution sample as input
-    decoder_input = Input(K.int_shape(z)[1:])
     x = Dense(
         3200, activation="relu", name="intermediate_decoder", input_shape=(latent_dim,)
-    )(decoder_input)
+    )(x)
     x = Reshape((5, 5, 128))(x)
 
     x = Conv2DTranspose(128, (3, 3), strides=2, padding="same")(x)
@@ -69,33 +132,10 @@ def vae():
 
     x = Conv2DTranspose(3, (3, 3), strides=2, padding="same", activation="sigmoid")(x)
 
-    # decoder model statement
-    decoder = Model(decoder_input, x)
+    # AE model statement
+    ae = Model(input_img, x, name="AE")
 
-    # apply the decoder to the sample from the latent distribution
-    pred = decoder(z)
-
-    def vae_loss(x, pred):
-        x = K.flatten(x)
-        pred = K.flatten(pred)
-        # Reconstruction loss
-        reconst_loss = 1000 * K.mean(K.square(x - pred))
-
-        # KL divergence
-        kl_loss = -0.5 * K.mean(
-            1 + z_log_sigma - K.square(z_mu) - K.exp(z_log_sigma), axis=-1
-        )
-
-        return reconst_loss + kl_loss
-
-    # VAE model statement
-    vae = Model(input_img, pred, name="VAE")
-    vae.add_loss(vae_loss(input_img, pred))
-    # optimizer = Adam(learning_rate=0.0005)
-    # vae.compile(optimizer=optimizer, loss=None)
-
-    # vae.summary()
-    return vae
+    return ae
 
 
 def generate_fake_samples(vae_model, latent_dim, n_samples, dataset):
@@ -151,11 +191,11 @@ def train(
 def main():
     dataset = load_real_samples()
     d_model = define_discriminator()
-    vae_model = vae()
+    ae_model = ae()
 
-    gan_model = define_gan(vae_model, d_model)
+    gan_model = define_gan(ae_model, d_model)
 
-    train(vae_model, d_model, gan_model, dataset, 128)
+    train(ae_model, d_model, gan_model, dataset, 128)
 
 
 if __name__ == "__main__":
