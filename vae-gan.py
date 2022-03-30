74952aa8bd616caff70b939c1e10749bf9865b3e
Kevyn Kelso <vyn@Kevyns-MacBook-Pro.local>
2022-03-29 19:22:56 -0600

    fix: uptate vaegan loss function

--- vae-gan.py
+++ vae-gan.py
@@ -1,10 +1,12 @@
 import matplotlib as mpl
+from tensorflow.keras.callbacks import EarlyStopping
+import tensorflow.keras.backend as K
 from tensorflow.python.keras.engine import data_adapter
 from tensorflow.keras.losses import BinaryCrossentropy, MeanSquaredError
 from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
 
-mpl.use("Agg")  # Disable the need for X window environment
+# mpl.use("Agg")  # Disable the need for X window environment
 from matplotlib import pyplot
 from numpy import ones, zeros
 from numpy.random import randint
@@ -13,14 +15,14 @@ from tensorflow.keras.layers import (BatchNormalization, Conv2D,
                                      Lambda, LeakyReLU, Reshape)
 from tensorflow.keras.models import Sequential
 
-from gan import (define_discriminator, define_generator,
+from gan import (define_discriminator, 
                  generate_real_samples, load_real_samples)
 
 
 # AE VERSION 2, based almost entirely on discriminator / generator
 def ae(in_shape=(80, 80, 3)):
     latent_dim = 100
-    model = Sequential(name="Encoder")
+    model = Sequential(name="Autoencoder")
     # normal
     model.add(Conv2D(128, (5, 5), padding="same", input_shape=in_shape))
     model.add(LeakyReLU(alpha=0.2))
@@ -39,7 +41,25 @@ def ae(in_shape=(80, 80, 3)):
     # classifier
     model.add(Flatten())
     model.add(Dense(latent_dim, activation="relu"))
-    model.add(define_generator(latent_dim))
+    # foundation for 5x5 feature maps
+    n_nodes = 100 * 5 * 5
+    model.add(Dense(n_nodes, input_dim=latent_dim))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Reshape((5, 5, 100)))
+    # upsample to 10x10
+    model.add(Conv2DTranspose(128, (4, 4), strides=(2, 2), padding="same"))
+    model.add(LeakyReLU(alpha=0.2))
+    # upsample to 20x20
+    model.add(Conv2DTranspose(128, (4, 4), strides=(2, 2), padding="same"))
+    model.add(LeakyReLU(alpha=0.2))
+    # upsample to 40x40
+    model.add(Conv2DTranspose(128, (4, 4), strides=(2, 2), padding="same"))
+    model.add(LeakyReLU(alpha=0.2))
+    # upsample to 80x80
+    model.add(Conv2DTranspose(128, (4, 4), strides=(2, 2), padding="same"))
+    model.add(LeakyReLU(alpha=0.2))
+    # output layer 80x80x3
+    model.add(Conv2D(3, (5, 5), activation="tanh", padding="same"))
 
     return model
 
@@ -106,10 +126,6 @@ def train(ae_model, d_model, gan_model, dataset, n_epochs=100, n_batch=13):
             X_real, y_real = generate_real_samples(dataset, half_batch)
             # update discriminator model weights
             d_loss1, _ = d_model.train_on_batch(X_real, y_real)
-
-            # updae AE model weights
-            # ae_loss1, _ = ae_model.train_on_batch(X_real, X_real)
-
             # generate 'fake' examples
             X_fake, y_fake = generate_fake_samples(ae_model, half_batch, dataset)
             # update discriminator model weights
@@ -123,7 +139,8 @@ def train(ae_model, d_model, gan_model, dataset, n_epochs=100, n_batch=13):
             g_loss = gan_model.train_on_batch(X_gan, y_gan)
             # summarize loss on this batch
             print(
-                f">{i+1}, {j+1}/{bat_per_epo}, d1={d_loss1:.3f}, d2={d_loss2:.3f}, g={g_loss:.3f}"
+                # f">{i+1}, {j+1}/{bat_per_epo}, d1={d_loss1:.3f}, d2={d_loss2:.3f}, g={g_loss:.3f}"
+                f">{i+1}, {j+1}/{bat_per_epo}, d1={d_loss1:.3f}, d2={d_loss2:.3f}"
             )
         # evaluate the model performance, sometimes
         # if (i + 1) % 10 == 0:
@@ -137,6 +154,15 @@ def define_gan(g_model, d_model):
     model = VAEGAN(g_model)
     # add generator
     model.add(g_model)
+
+    def ae_loss(x, pred):
+        x = K.flatten(x)
+        pred = K.flatten(pred)
+        reconst_loss = 1000 * K.mean(K.square(x - pred))
+        return reconst_loss
+
+    model.add_loss(ae_loss(model.input, model.output))
+
     # add the discriminator
     model.add(d_model)
     # compile model
@@ -154,14 +180,14 @@ def loss_wapper(g_model, alpha, beta):
         y = g_model(x)
         ae_loss = alpha * mse(x, y)
         gan_loss = beta * bce(y_true, y_pred)
-        print(f"ae_loss = {ae_loss.numpy()} gan_loss = {gan_loss}")
+        print(f"ae_loss = {ae_loss} gan_loss = {gan_loss}")
         return ae_loss + gan_loss
 
     return loss
 
 class VAEGAN(tf.keras.Sequential):
-    def compile(self, optimizer, my_loss):
-        super().compile(optimizer)
+    def compile(self, optimizer, my_loss, run_eagerly=True):
+        super().compile(optimizer, run_eagerly=True)
         self.my_loss = my_loss
 
     def train_step(self, data):
@@ -175,16 +201,34 @@ class VAEGAN(tf.keras.Sequential):
         grads = tape.gradient(loss_value, self.trainable_variables)
         self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
 
-        return {"loss_value": loss_value}
+        return {"loss": loss_value}
 
 def main():
     dataset = load_real_samples()
-    d_model = define_discriminator()
+    # d_model = define_discriminator()
     ae_model = ae()  # AE model is generator
+    early_stopping = EarlyStopping(
+        monitor="loss", min_delta=0, patience=10, verbose=5, mode="auto"
+    )
+    opt = Adam(lr=0.0002, beta_1=0.5)
+    ae_model.compile(optimizer=opt, loss='mse')
 
-    gan_model = define_gan(ae_model, d_model)
+    ae_model.fit(x=dataset, y=dataset, epochs=10, batch_size=1, callbacks=[early_stopping])
+    ae_model.save("ae_generator_1.h5")
+    output_imgs = ae_model(dataset)
+    output_imgs = (output_imgs + 1) / 2.0
+    for i in range(20):
+        pyplot.subplot(5, 4, i +1)
+        pyplot.axis("off")
+        pyplot.imshow(output_imgs[i])
+    pyplot.savefig("ae_training_test.png")
+    pyplot.close()
 
-    train(ae_model, d_model, gan_model, dataset)
+
+
+    # gan_model = define_gan(ae_model, d_model)
+
+    # train(ae_model, d_model, gan_model, dataset)
 
 
 if __name__ == "__main__":
